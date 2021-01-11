c2c226c670554c6165b98bb78816ac3f24476038
Dorcas Temitope Emmanuel <portabledd@pop-os.localdomain>
2021-01-11 12:20:09 +0100

    done with it

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function() {
   'use strict';
@@ -18,3 +17,51 @@
   }, false);
 })();
 
+// function submitForm() {
+//   validateFields()
+//   if (!errorMessages.length) {
+//       const { email, phone, amount } = getUserDetails()
+
+//       payWithPaystack(email,amount,phone)
+//   }
+// }
+
+// function getUserDetails() {
+//   const name = document.querySelector('.name input').value
+//   const phone = document.querySelector('.phone input'). value
+//   const email = document.querySelector('.email input'). value
+//   const house = document.querySelector('.house input'). value
+//   const size = document.querySelector('.size input'). value
+//   const amount = document.querySelector('#price').innerText
+//   return {name, phone, email, house, size, amount}
+// }
+
+// function validateFields() {
+//   const {name, phone} = getUserDetails()
+
+//   if (name.length < 3){
+//       errorMessages.push('Incomplete name entererd')
+//   }
+//   if (isNaN(phone)){
+//       errorMessages.push('Invalid phone number entered')
+//   }
+
+//   if(errorMessages.length) {
+//       let el = ''
+//       errorMessages.forEach(err => {
+//           el += `<p>${err}</p>`
+//       })
+//       return document.querySelector('.error').innerHTML = el
+//   }
+// }
+
+
+// function clearError() {
+//   let elements = document.querySelectorAll('.field')
+//   elements.forEach(el => {
+//       if(errorMessages.length){
+//           errorMessages = []
+//           document.querySelector('.error').innerHTML = ''
+//       }
+//   })
+// }
